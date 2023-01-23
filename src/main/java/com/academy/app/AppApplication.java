cf60a96e4c3fa5f951d4485d7ccb766506dee299
sokol-forbes <sasha.sokolovsky01@gmail.com>
2023-01-23 21:16:17 +0300

    fix annotations

--- src/main/java/com/academy/app/AppApplication.java
+++ src/main/java/com/academy/app/AppApplication.java
@@ -1,11 +1,26 @@
 package com.academy.app;
 
+import com.academy.app.dto.converter.ProductCategoryConverter;
+import com.academy.app.dto.converter.ProductConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
     public static void main(String[] args)  {
+
         SpringApplication.run(AppApplication.class, args);
+
+
+    }
+    @Bean
+    public ProductConverter createProductConverter() {
+        return new ProductConverter();
+    }
+
+    @Bean
+    public ProductCategoryConverter createProductCategoryConverter() {
+        return new ProductCategoryConverter();
     }
 }
